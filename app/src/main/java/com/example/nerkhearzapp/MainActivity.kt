ed7320e66754f928293c4221cd2c22f86bfe95ef
mohammadmahdi <hamdimohammadmahdi@gmail.com>
2024-06-07 23:46:27 +0330

    tab layout added

--- app/src/main/java/com/example/nerkhearzapp/MainActivity.kt
+++ app/src/main/java/com/example/nerkhearzapp/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.nerkhearzapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.nerkhearzapp.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +13,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //راست چین
+        binding.tabLayout.getTabAt(1)
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "" + tab?.position, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+        })
     }
 }
